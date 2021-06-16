9e03fb9dd5b327b559c2c6789a328e76e4fbdbe6
Ben Baldieri <ben.baldieri@gmail.com>
2021-06-16 08:57:06 +0800

    Name and description field added to devise user signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.save
-
-    redirect_to user_path(@user)
   end
 
   def edit
@@ -27,13 +12,6 @@ class UsersController < ApplicationController
     @user.update(user_params)
 
     redirect_to user_path(@user)
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    redirect_to users_path
   end
 
   private
