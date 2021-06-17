c3e77bdce53c3a535b633a0470e2605075e354a0
Ben Baldieri <ben.baldieri@gmail.com>
2021-06-17 19:43:32 +0800

    navbar added, devise pages generated

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users, only: [:show, :edit, :update]
 end
