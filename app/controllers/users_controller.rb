720f7208efe167f9360a05d5fee598a1b9dbbdc8
Ben Baldieri <ben.baldieri@gmail.com>
2021-06-19 17:28:37 +0800

    controller returned to previous

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,6 @@
 class UsersController < ApplicationController
   def show
-    if params[:id]
       @user = User.find(params[:id])
-
-      redirect_to profile_path
-    else
-      @user = current_user
-
-      redirect_to profile_path
-    end
   end
 
   def edit
