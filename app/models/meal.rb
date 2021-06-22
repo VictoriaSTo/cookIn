afda21ec8e3acdaf6f9c91b0bdabb9e04e2ae410
Ben Baldieri <ben.baldieri@gmail.com>
2021-06-22 20:28:42 +0800

    search bar with pg search added

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,4 +4,10 @@ class Meal < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, :in => 10..500 }
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
